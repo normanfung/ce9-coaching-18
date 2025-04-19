9c4dc2617a5ac9d0fd87192216789c9721b8a512
normanfung96 <56857601+normanfung@users.noreply.github.com>
2025-04-19 11:47:57 +0800

    infra: updated csr

--- terraform/stage1-infra/csr.tf
+++ terraform/stage1-infra/csr.tf
@@ -1,5 +1,5 @@
-resource "aws_ecr_repository" "app_repo" {
-  name                 = "${local.prefix}-csr"
+resource "aws_ecr_repository" "s3_app_repo" {
+  name                 = "${local.prefix}-csr-s3-app"
   image_tag_mutability = "MUTABLE" # or "IMMUTABLE" for stricter image control
   encryption_configuration {
     encryption_type = "AES256"
@@ -7,8 +7,43 @@ resource "aws_ecr_repository" "app_repo" {
   force_delete = true
 }
 
-resource "aws_ecr_repository_policy" "csr_policy" {
-  repository = aws_ecr_repository.app_repo.name
+resource "aws_ecr_repository" "sqs_app_repo" {
+  name                 = "${local.prefix}-csr-sqs-app"
+  image_tag_mutability = "MUTABLE" # or "IMMUTABLE" for stricter image control
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+  force_delete = true
+}
+
+resource "aws_ecr_repository_policy" "csr_policy_s3" {
+  repository = aws_ecr_repository.s3_app_repo.name
+
+  policy = jsonencode({
+    Version = "2008-10-17"
+    Statement = [
+      {
+        Sid    = "AllowPushPull"
+        Effect = "Allow"
+        Principal = {
+          AWS = "*"
+        }
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_ecr_repository_policy" "csr_policy_sqs" {
+  repository = aws_ecr_repository.sqs_app_repo.name
 
   policy = jsonencode({
     Version = "2008-10-17"
