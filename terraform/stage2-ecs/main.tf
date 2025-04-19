a670d213a5ced44506e79c02bd69cd0240ea6431
normanfung96 <56857601+normanfung@users.noreply.github.com>
2025-04-19 12:56:30 +0800

    infra: update task

--- terraform/stage2-ecs/main.tf
+++ terraform/stage2-ecs/main.tf
@@ -56,6 +56,24 @@ resource "aws_ecs_cluster" "main" {
   name = "${local.prefix}-fargate-cluster"
 }
 
+resource "aws_iam_role" "ecs_task_role" {
+  name = "${local.prefix}-ecsTaskRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+
 # IAM Role for Fargate
 resource "aws_iam_role" "ecs_task_execution" {
   name = "${local.prefix}-ecsTaskExecutionRole"
@@ -72,6 +90,12 @@ resource "aws_iam_role" "ecs_task_execution" {
       }
     ]
   })
+}
+
+resource "aws_iam_policy_attachment" "ecs_app_task_policy_attach" {
+  name       = "${local.prefix}-app-policy-attach"
+  roles      = [aws_iam_role.ecs_task_role.name]
+  policy_arn = aws_iam_policy.ecs_app_policy.arn
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_execution_attach" {
@@ -107,11 +131,6 @@ resource "aws_iam_policy" "ecs_app_policy" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_app_policy_attach" {
-  role       = aws_iam_role.ecs_task_execution.name
-  policy_arn = aws_iam_policy.ecs_app_policy.arn
-}
-
 resource "aws_ecs_task_definition" "multi_app_task" {
   family                   = "multi-app-task"
   requires_compatibilities = ["FARGATE"]
@@ -119,6 +138,8 @@ resource "aws_ecs_task_definition" "multi_app_task" {
   cpu                      = "512"
   memory                   = "1024"
   execution_role_arn       = aws_iam_role.ecs_task_execution.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+  depends_on               = [aws_cloudwatch_log_group.ecs_logs]
 
   container_definitions = jsonencode([
     {
